c4043047d92b8259b28730323fc528d3bf640383
Matt Bruton <mb@deliowealth.com>
2017-01-20 14:44:29 +0000

    Added langauge models

--- classes/bundle_language.php
+++ classes/bundle_language.php
@@ -7,8 +7,13 @@ namespace adapt\language{
     
     class bundle_language extends \adapt\bundle{
         
+        protected $_strings;
+        
         public function __construct($data){
             parent::__construct('language', $data);
+            $this->_strings = [];
+            
+            $this->register_config_handler('language', 'language_strings', 'process_language_strings_tag');
         }
         
         public function boot(){
@@ -36,6 +41,45 @@ namespace adapt\language{
                 return true;
             }
             return false;
+        }
+        
+        public function process_language_strings_tag($bundle, $tag_data){
+            if ($bundle instanceof \adapt\bundle && $tag_data instanceof \adapt\xml){
+                $this->register_install_handler($this->name, $bundle->name, 'install_strings');
+                
+                $string_nodes = $tag_data->get();
+                $this->_strings[$bundle->name] = [];
+                
+                foreach($string_nodes as $string_node){
+                    if ($string_node instanceof \adapt\xml && $string_node->tag == 'string'){
+                        if (!is_array($this->_strings[$bundle->name][$string_node->attr('language')])){
+                            $this->_strings[$bundle->name][$string_node->attr('language')] = [];
+                        }
+                        $this->_strings[$bundle->name][$string_node->attr('language')][] = [
+                            'language' => $string_node->attr('language'),
+                            'key' => $string_node->attr('key'),
+                            'value' => $string_node->attr('value')
+                        ];
+                    }
+                }
+            }
+        }
+        
+        public function install_strings($bundle){
+            $bundle_strings = $this->_strings[$bundle->name];
+            
+            if (is_array($bundle_strings)){
+                foreach($bundle_strings as $language => $strings){
+                    $model_language = new model_language();
+                    if ($model_language->load_by_name($language)){
+                        foreach($strings as $string){
+                            $model_language->register_string($string['key'], $string['value']);
+                        }
+                    }
+                }
+            }
+            print_r($this->_strings);
+            exit(1);
         }
         
     }
---
+++ models/model_language.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace adapt\language{
+    
+    class model_language extends \adapt\model {
+        
+        public function __construct($id = null, $data_source = null) {
+            parent::__construct('language', $id, $data_source);
+        }
+        
+        public function register_string($key, $value){
+            if (!$this->is_loaded){
+                $this->error('Language not loaded');
+                return false;
+            }
+            
+            $cache_key = "adapt/language/" . $this->name;
+            $strings = $this->cache->get($cache_key);
+            if (!is_array($strings)) $strings = [];
+            $hash_key = md5($key);
+            
+            if (in_array($hash_key, array_keys($strings))){
+                $strings[$hash_key] = $value;
+            }else{
+                // Does the key exist?
+                $model_key = new model_language_key();
+                if ($model_key->load_by_name($key)){
+                    // Does the value exist?
+                    $sql = $this->data_source->sql;
+                    
+                    $sql->select('v.value')
+                        ->from('language_key', 'k')
+                        ->join(
+                            'language_value', 
+                            'v',
+                            new sql_and(
+                                new sql_cond('k.language_id', sql::EQUALS, 'v.language_key_id'),
+                                new sql_cond('v.language_id', sql::EQUALS, $this->language_id),
+                                new sql_cond('v.date_delete', sql::IS, new sql_null())
+                            )
+                        )
+                        ->where(
+                            new sql_and(
+                                new sql_cond('k.name', sql::EQUALS, q($key)),
+                                new sql_cond('k.date_deleted', sql::IS, new sql_null())
+                            )
+                        );
+
+                    $results = $sql->execute()->results();
+
+                    if (count($results) == 1){
+                        $value = $results[0]['value'];
+                        $strings[$hash_key] = $value;
+                    }elseif(count($results) == 0){
+                        $model_value = new model_language_value();
+                        
+                        $model_value->language_key_id = $model_key->language_key_id;
+                        $model_value->language_id = $this->language_id;
+                        $model_value->value = $value;
+
+                        if (!$model_value->save()){
+                            $this->error("Unable to set key value");
+                            return false;
+                        }
+
+                        // Update the strings array
+                        $strings[$hash_key] = $value;
+                    }
+                }else{
+                    // Clear the load error
+                    $model_key->errors(true);
+                    
+                    // New key
+                    $model_key->name = $key;
+                    
+                    if (!$model_key->save()){
+                        $model_key->errors(true);
+                        $this->error('Unable to create language key ' . $key);
+                        return false;
+                    }
+                    
+                    $model_value = new model_language_value();
+                    $model_value->language_key_id = $model_key->language_key_id;
+                    $model_value->language_id = $this->language_id;
+                    $model_value->value = $value;
+                    
+                    if (!$model_value->save()){
+                        $this->error("Unable to set key value");
+                        return false;
+                    }
+                    
+                    // Update the strings array
+                    $strings[$hash_key] = $value;
+                }
+            }
+            
+            // Re-cache
+            $this->cache->serialize($cache_key, $strings, 60 * 60 * 24 * 365);
+            return true;
+        }
+        
+        public function get_string($key){
+            if (!$this->is_loaded){
+                return $key;
+            }
+            
+            $cache_key = "adapt/language/" . $this->name;
+            $strings = $this->cache->get($cache_key) || [];
+            $hash_key = md5($key);
+            
+            if (in_array($hash_key, array_keys($strings))){
+                return $strings[$key];
+            }else{
+                // Check the database then add to cache
+                $sql = $this->data_source->sql;
+                $sql->select('v.value')
+                    ->from('language_key', 'k')
+                    ->join(
+                        'language_value', 
+                        'v',
+                        new sql_and(
+                            new sql_cond('k.language_id', sql::EQUALS, 'v.language_key_id'),
+                            new sql_cond('v.language_id', sql::EQUALS, $this->language_id),
+                            new sql_cond('v.date_delete', sql::IS, new sql_null())
+                        )
+                    )
+                    ->where(
+                        new sql_and(
+                            new sql_cond('k.name', sql::EQUALS, q($key)),
+                            new sql_cond('k.date_deleted', sql::IS, new sql_null())
+                        )
+                    );
+                
+                $results = $sql->execute()->results();
+                
+                if (count($results) == 1){
+                    $value = $results[0]['value'];
+                    $strings[$hash_key] = $value;
+                    // Re-cache the strings
+                    $this->cache->serialize($cache_key, $strings, 60 * 60 * 24 * 365);
+                    
+                    return $value;
+                }
+            }
+            
+            // Incase all else fails
+            return $key;
+        }
+    }
+
+}
